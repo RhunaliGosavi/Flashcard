f6fe2eb0d041e39c90ce5da92065456f8f170539
Rhunali Gosavi <rhuna0606@gmail.com>
2023-09-13 15:28:54 +0530

    initial commit

---
+++ src/FlashcardList.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import FlashCard from './FlashCard'
+
+export default function FlashcardList({flashcards}) {
+
+ 
+  return (
+    <div className='card-grid'>
+        {flashcards.map((flashcard)=>{
+            
+            return <FlashCard  flashcard ={flashcard} key={flashcard.key}/>
+        })}
+      
+    </div>
+  )
+}
